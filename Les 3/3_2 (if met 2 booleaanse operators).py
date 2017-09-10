d0d921ca4257d488562e3170cb8d5166baef40a9
Achraf-Ben <achraf.ben.a@outlook.com>
2017-09-10 19:57:47 +0200

    Les 3
    Signed-off-by: Achraf-Ben <achraf.ben.a@outlook.com>

---
+++ Les 3/3_2 (if met 2 booleaanse operators).py
@@ -0,0 +1,6 @@
+score = input('Geef je leeftijd : ')
+print(score)
+passport = input('Nederlands paspoort: ')
+
+if int(score) >= 18 and passport == "ja" or passport == "Ja":
+        print('Gefeliciteerd je mag stemmen!')
