17e3db59a31b591087c9cbfd71ec3270f89031ee
Achraf-Ben <achraf.ben.a@outlook.com>
2017-09-18 00:20:48 +0200

    Opdrachten Les 4
    Signed-off-by: Achraf-Ben <achraf.ben.a@outlook.com>

---
+++ Opdrachten Les 4/4_5 (functie +  list parameter + foor loop).py
@@ -0,0 +1,9 @@
+import math
+
+def kwadraten_som():
+    grondgetallen = [4,5,3,-81]
+    for getal in grondgetallen:
+        if getal >= 0:
+            sqrt(getal)
+            print(getal)
+# Niet af
