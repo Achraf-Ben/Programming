3b2980f05ff931fef673a1e5bb001706afce8753
Achraf-Ben <achraf.ben.a@outlook.com>
2017-09-18 16:09:14 +0200

    Opdrachten Les 4
    Signed-off-by: Achraf-Ben <achraf.ben.a@outlook.com>

--- Opdrachten Les 4/FA (NS-functies).py
+++ Opdrachten Les 4/FA (NS-functies).py
@@ -13,20 +13,20 @@ def standaardtarief(afstandKM): #standaardbedrag voor een rit
 def ritprijs(leeftijd, weekendrit, afstandKM): #return waarde is definitieve prijs
     tarief = standaardtarief(afstandKM)
 
-    if leeftijd <= 11 or leeftijd >= 65 and weekendrit is 1:  # weekend + -12 en 65+ korting
-        tarief = tarief * 0.65   #tarief * 0.70 = 35% korting
+    if leeftijd <= 11 or leeftijd >= 65 and weekendrit is 1:
+        tarief = tarief * 0.65
         print('65+ Weekendtarief:')
-        print(tarief) # print kortingstarief 65+
-    elif leeftijd <= 11 or leeftijd >= 65 and weekendrit is 0:  # -12 en 65+ korting
-        tarief = tarief * 0.70   #tarief * 0.70 = 30% korting
+        print(tarief)
+    elif leeftijd <= 11 or leeftijd >= 65 and weekendrit is 0:
+        tarief = tarief * 0.70
         print('65+ Kortingstarief')
-        print(tarief) # print kortingstarief 65+
+        print(tarief)
     elif weekendrit == 1:
-        tarief = tarief * 0.60  # tarief * 0.60 = 40% korting
+        tarief = tarief * 0.60
         print('Weekendtarief:')
-        print(tarief)  # print kortingstarief 65+
+        print(tarief)
     else:
         print('Normaaltarief:')
-        print(tarief) # print standaardtarief
+        print(tarief)
 
 ritprijs(64,0,-10)
