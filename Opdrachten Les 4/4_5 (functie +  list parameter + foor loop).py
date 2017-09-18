2b51ae9b158fd1ed3a4147d1cae915bca9025f69
Achraf-Ben <achraf.ben.a@outlook.com>
2017-09-18 14:07:37 +0200

    Opdrachten Les 4
    Signed-off-by: Achraf-Ben <achraf.ben.a@outlook.com>

--- Opdrachten Les 4/4_5 (functie +  list parameter + foor loop).py
+++ Opdrachten Les 4/4_5 (functie +  list parameter + foor loop).py
@@ -1,10 +1,9 @@
-import math
-
 def kwadraten_som(grondgetallen):
     uitkomst = 0;
     for getal in grondgetallen:
         if (getal >= 0):
             uitkomst = uitkomst + getal*getal
     return uitkomst
+
 
 print(kwadraten_som([4, 5, 3, -81]))
