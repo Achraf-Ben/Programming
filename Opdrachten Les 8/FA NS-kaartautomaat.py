0401623dfc908c4eb6eafe0356e32d5fa1de8b89
Achraf-Ben <achraf.ben.a@outlook.com>
2017-10-02 14:32:27 +0200

    FA les 8

--- Opdrachten Les 8/FA NS-kaartautomaat.py
+++ Opdrachten Les 8/FA NS-kaartautomaat.py
@@ -1,14 +1,14 @@
 def inlezen_beginstation(stations):
-    while True:
-        invoer = input('Voer het beginstation in: ')
+   invoer = input('Voer het beginstation in: ')
+   while True:
         if invoer in stations:
             return invoer
         else:
             invoer = input('Voer het beginstation in: ')
 
 def inlezen_eindstation(stations, beginstation):
+    invoer = input('Voer het eindstation in: ')
     while True:
-        invoer = input('Voer het eindstation in: ')
         if invoer in stations:
             if stations.index(invoer) >= stations.index(beginstation):
                 return invoer
@@ -16,17 +16,19 @@ def inlezen_eindstation(stations, beginstation):
             invoer = input('Voer het eindstation in: ')
 
 def omroepen_reis(stations, beginstation, eindstation):
-    afstand = stations.index(eindstation) - stations.index(beginstation)
+    beginStation = stations.index(beginstation)+ 1
+    eindStation = stations.index(eindstation) + 1
+    afstand = eindStation - beginStation
     prijs = 5 * afstand
-    print('Het beginstaton {0} is het {1}e station in het traject'.format(beginstation,stations.index(beginstation)))
-    print('Het eindstation {0} is het {1}e station in het traject'.format(eindstation, stations.index(eindstation)))
+    print('Het beginstaton {0} is het {1}e station in het traject'.format(beginstation,beginStation))
+    print('Het eindstation {0} is het {1}e station in het traject'.format(eindstation,eindStation))
     print('De afstand bedraagt {0} stations'.format(afstand))
     print('De prijs van het kaartje is {0} euro'.format(prijs))
     return
 
 
 stations = ['Schagen', 'Heerhugowaard', 'Alkmaar', 'Castricum', 'Zaandam', 'Amsterdam Sloterdijk', 'Amsterdam Centraal',
-            'Amsterdam  Amstel', 'Utrecht Centraal', '’s-Hertogenbosch', 'Eindhoven', 'Weert', 'Roermond', 'Sittard', 'Maastricht']
+            'Amsterdam  Amstel', 'Utrecht Centraal', '\'s-Hertogenbosch', 'Eindhoven', 'Weert', 'Roermond', 'Sittard', 'Maastricht']
 beginstation = inlezen_beginstation(stations)
 eindstation = inlezen_eindstation(stations, beginstation)
 
